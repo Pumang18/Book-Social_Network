29898c7c8aabecceabff5ec13bfffd8f8aeaf1a8
Pumang Kirange <kirangespumang03@gmail.com>
2025-08-02 16:24:30 +0530

    added security filter

---
+++ book-network/src/main/java/com/pumag/book_network/security/UserDetailsServiceImpl.java
@@ -0,0 +1,26 @@
+package com.pumag.book_network.security;
+
+import com.pumag.book_network.user.UserRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public UserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String userEmail) throws UsernameNotFoundException {
+        return userRepository.findByEmail(userEmail)
+                .orElseThrow(()->new UsernameNotFoundException("User Not Found"));
+    }
+}
